efd0a6071543b4a76c01b0c06808a2cb304053a1
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-10-07 22:40:09 +0530

    Add files via upload

---
+++ Array1/src/ArrayExample/AddElement1.java
@@ -0,0 +1,22 @@
+package ArrayExample;
+
+import java.util.Arrays;
+
+public class AddElement1 {
+
+	
+	public static void main(String[] args) {
+		
+		String [] arr = {"omkar","dhanaji","mane","sangita", "pooja"};
+		
+		
+		System.out.println(arr);
+		
+		System.out.println(Arrays.toString(arr));
+		
+		
+		
+				
+		
+	}
+}
