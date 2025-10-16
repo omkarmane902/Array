1f3fb7b038992ab58ebf0d5ce6966c2ec3fafa62
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2025-10-16 12:19:18 +0530

    Add files via upload

---
+++ Array1/src/ArrayExample/bubble_sort.java
@@ -0,0 +1,33 @@
+package ArrayExample;
+
+import java.util.Arrays;
+
+public class bubble_sort {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		int [] arr = {10, 3, 12, 7, 2, 11, 9};
+		
+		
+		for(int i = 0 ; i < arr.length-1; i++)
+		{
+			for(int j = 0 ; j < arr.length-i-1; j++)
+			{
+				if(arr[j] > arr[j+1])
+				{
+					int temp = arr[j];
+					
+					arr[j] = arr[j +1];
+					
+					arr[j +1] = temp;
+				}
+				
+			}
+		}
+		
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
